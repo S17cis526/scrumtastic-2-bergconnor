0acc79b5574a8962f6e1abbc6d7df81363d06c3e
bergconnor <cberg1@ksu.edu>
2017-03-09 14:15:50 -0600

    fixed typos

--- app.js
+++ app.js
@@ -2,17 +2,26 @@
 
 
 var sqlite3 = require('sqlite3').verbose();
+var require('http');
 var db = new sqlite3.Database('scrumtastic.sqlite3', function(err) {
   if(err) console.error(err);
 });
 
+var router = new (require('./lib/route').Router);
+
+router.add('/app.js', function(req, res) {
+  fs.readFile('public/app.js', function(err, body) {
+    res.end(body);
+  });
+});
+
 var migrate = require('./lib/migrate');
-migrate(db, 'migrations', function(err){
-  db.serialize(function(){
-    db.run("INSERT INTO projects (name) values (?)", ['title']);
-    db.all("SELECT * FROM projects", [], function(err, rows){
-      if(err) return console.error(err);
-      console.log("ROWS:", rows);
-    });
+migrate(db, 'migrations', function(err) {
+
+  var server = new http.Server(function(req, res) {
+    router.route(req, res)
+  });
+  server.listen(3000, function() {
+    console.log("Listening on port " + 3000);
   });
 });
